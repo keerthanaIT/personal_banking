d06c7e9b10206ca079064cdc2214b788ef187f47
Keerthana <keerthanam119@gmail.com>
2020-12-24 11:40:23 +0530

    text color changes done in personal banking section

--- src/component/Dashboard/Overview/WeeklyStatistics.js
+++ src/component/Dashboard/Overview/WeeklyStatistics.js
@@ -12,19 +12,19 @@ class WeeklyStatistics extends React.Component {
 					<ul>
 						<li>
 							<span className="font-b dark-gray-color fs-14">Taxi</span>
-							<span className="float-right font-b dark-gray-color fs-14">68%</span>
+							<span className="float-right font-b light-gray-color fs-14">68%</span>
 							<Line percent="68" trailWidth="3" strokeWidth="3" strokeColor="#E44E42" />
 							<i className="fas fa-car user_icon_clr statistics_car_icon" />
 						</li>
 						<li>
 							<span className="font-b dark-gray-color fs-14">Games</span>
-							<span className="float-right font-b dark-gray-color fs-14">44%</span>
+							<span className="float-right font-b light-gray-color fs-14">44%</span>
 							<Line percent="44" trailWidth="3" strokeWidth="3" strokeColor="#F7D132" />
 							<i class="fas fa-gamepad user_icon_clr statistics_game_icon" />
 						</li>
 						<li>
 							<span className="font-b dark-gray-color fs-14">Study</span>
-							<span className="float-right font-b dark-gray-color fs-14">79%</span>
+							<span className="float-right font-b light-gray-color fs-14">79%</span>
 							<Line percent="79" trailWidth="3" strokeWidth="3" strokeColor="#6573ED" />
 							<i class="fas fa-graduation-cap user_icon_clr statistics_study_icon" />
 						</li>
